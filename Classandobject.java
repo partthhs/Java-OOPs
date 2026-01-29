652f7fdac12fcafc70a0e944127ddeb7b8294ff7
partthhs <parthh989@gmail.com>
2026-01-29 22:49:44 +0530

    Create Classandobject.java

---
+++ Classandobject.java
@@ -0,0 +1,20 @@
+class Student 
+{
+int id;
+String name;
+
+void display()
+{
+System.out.println(id+" "+name);
+}
+}
+public class Classandobject
+{
+public static void main(String[]args)
+{
+Student s1= new Student();
+s1.id = 62;
+s1.name = "Parth";
+s1.display();
+}
+}
