27d300cb78e3dcd9d9865d61da0361af9aff553e
partthhs <parthh989@gmail.com>
2026-01-30 18:54:40 +0530

    Create Comparemethod.java
    added code

---
+++ Comparemethod.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Comparemethod.java
+{
+public static void main(String[]args)
+{
+Scanner sc= new Scanner(System.in);
+
+ System.out.println("Enter two Numbers=");
+int a= sc.nextInt();
+int b= sc.nextInt();
+System.out.println(Integer.compare(a,b));
+
+System.out.println("Enter two numbers=");
+int x=sc.nextInt();
+int y= sc.nextInt();
+System.out.println(Integer.compare(x,y));
+
+System.out.println("Enter two numbers=");
+int p= sc.nextInt();
+int q= sc.nextInt();
+System.out.println(Integer.compare(p,q));
+  }
+}
