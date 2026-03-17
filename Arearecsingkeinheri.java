c36b79f02baaffd2d04c934f5946e512d69a3669
Parth Sakpal <parthh989@gmail.com>
2026-03-17 23:57:11 +0530

    Create Arearecsingleinheri.java

---
+++ Arearecsingkeinheri.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Rectangle {
+    int length, breadth;
+
+    void getData() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter length: ");
+        length = sc.nextInt();
+        System.out.print("Enter breadth: ");
+        breadth = sc.nextInt();
+    }
+}
+
+class Area extends Rectangle {
+    void calculateArea() {
+        int area = length * breadth;
+        System.out.println("Area of Rectangle: " + area);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Area obj = new Area();
+        obj.getData();
+        obj.calculateArea();
+    }
+}
