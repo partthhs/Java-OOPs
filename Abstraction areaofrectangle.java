60545e1ec714450d4d38819462aefcbd4ed0be37
Parth Sakpal <parthh989@gmail.com>
2026-03-28 21:14:10 +0530

    Create Abstraction areaofrectangle.java

---
+++ Abstraction areaofrectangle.java
@@ -0,0 +1,31 @@
+abstract class Shape {
+    abstract void area();
+}
+
+class Circle extends Shape {
+    double r = 5;
+
+    void area() {
+        System.out.println("Area of Circle: " + (3.14 * r * r));
+    }
+}
+
+class Rectangle extends Shape {
+    int l = 4, b = 6;
+
+    void area() {
+        System.out.println("Area of Rectangle: " + (l * b));
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Shape s;
+
+        s = new Circle();
+        s.area();
+
+        s = new Rectangle();
+        s.area();
+    }
+}
