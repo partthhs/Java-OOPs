fe52e09ad5c60ef4e4c97e3f0793b8d6cd71e7d0
Parth Sakpal <parthh989@gmail.com>
2026-04-05 22:24:44 +0530

    Create Area interface.java

---
+++ Area interface.java
@@ -0,0 +1,38 @@
+interface Shape {
+    double area();
+}
+
+class Circle implements Shape {
+    double r;
+
+    Circle(double r) {
+        this.r = r;
+    }
+
+    public double area() {
+        return Math.PI * r * r;
+    }
+}
+
+class Rectangle implements Shape {
+    double l, b;
+
+    Rectangle(double l, double b) {
+        this.l = l;
+        this.b = b;
+    }
+
+    public double area() {
+        return l * b;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Shape s1 = new Circle(5);
+        Shape s2 = new Rectangle(4, 6);
+
+        System.out.println("Circle Area = " + s1.area());
+        System.out.println("Rectangle Area = " + s2.area());
+    }
+}
