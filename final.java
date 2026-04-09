143eb2a6e40b6ec9148b797ae2491d0befffdab9
Parth Sakpal <parthh989@gmail.com>
2026-04-09 23:06:16 +0530

    Create final.java

---
+++ final.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class SimpleInterest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+       
+        final double RATE = 5.0;
+
+        System.out.print("Enter Principal: ");
+        double p = sc.nextDouble();
+
+        System.out.print("Enter Time (years): ");
+        double t = sc.nextDouble();
+
+       
+        double si = (p * RATE * t) / 100;
+
+        System.out.println("Simple Interest = " + si);
+
+        sc.close();
+    }
+}
